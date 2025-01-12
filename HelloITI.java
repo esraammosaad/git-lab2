9641cc84abe4f746692effac3a63cd30dd9e3a84
arwakhaled24 <110431648+arwakhaled24@users.noreply.github.com>
2025-01-12 06:04:29 +0200

    edit in helloITI

--- HelloITI.java
+++ HelloITI.java
@@ -6,9 +6,15 @@ public class HelloITI{
 		
 		System.out.println("Hello ITI!");
 		System.out.println("Hello Git");
+<<<<<<< Updated upstream
 		System.out.println("Hello arwa");
 
 		
+=======
+		System.out.println("new arwa branch");
+		]System.out.println("new arwa branch into development");
+
+>>>>>>> Stashed changes
 		
 		
 	}
